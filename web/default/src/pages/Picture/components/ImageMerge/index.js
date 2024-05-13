58fe255ea48128de377cab27ea0022dfa32e2421
goodhzy <goodhzy@gmail.com>
2024-05-13 08:33:39 +0800

    增加缩放比例

--- web/default/src/pages/Picture/components/ImageMerge/index.js
+++ web/default/src/pages/Picture/components/ImageMerge/index.js
@@ -14,25 +14,30 @@ const ImageMerge = (props, ref) => {
       const imageList = [...newVal];
       const canvas = document.getElementById('canvas');
       const ctx = canvas.getContext('2d');
+      // 缩放比例
+      let scale = 0;
+      // 固定宽高
+      const width = 1000;
+      const height = 1000;
       for (let i = 0; i < imageList.length; i++) {
         const image = imageList[i];
         const front_image = await getImageInfo(image.front.url);
         const back_image = await getImageInfo(image.back.url);
-        const max_width = getMax(front_image.width, back_image.width);
+        const max_width = getMax(front_image.width, back_image.width)
         const max_height = getMax(front_image.height, back_image.height);
-        canvas.width = max_width * 2;
-        canvas.height = max_height;
+        scale = max_width / width > max_height / height ? width / max_width : height / max_height;
+        canvas.width = max_width * 2 * scale
+        canvas.height = max_height * scale;
         console.log(max_width, max_height);
-        console.log(ctx);
-        // create HTMLImageElement
+        console.log(scale);
         const front_img = new Image();
         front_img.src = image.front.url;
         const back_img = new Image();
         back_img.src = image.back.url;
         back_img.onload = () => {
-          ctx.drawImage(front_img, 0, 0, front_image.width, front_image.height);
-          ctx.drawImage(back_img, front_image.width, 0, back_image.width, back_image.height);
-          image.result = canvas.toDataURL('jpg', 0.1);
+          ctx.drawImage(front_img, 0, 0, front_image.width * scale, front_image.height * scale);
+          ctx.drawImage(back_img, front_image.width * scale, 0, back_image.width * scale, back_image.height * scale);
+          image.result = canvas.toDataURL("image/jpeg", 0.9);
         }
       }
       setImageList(imageList);
