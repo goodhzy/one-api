9e50f2210f1d845ec7f2837b0ca44fc2cd63dfbd
ornnxp1 <1693120570@qq.com>
2024-05-28 18:56:47 +0800

    11

---
+++ web/berry/src/utils/api.js
@@ -0,0 +1,29 @@
+import { showError } from './common';
+import axios from 'axios';
+import { store } from 'store/index';
+import { LOGIN } from 'store/actions';
+import config from 'config';
+
+console.log(process.env.REACT_APP_SERVER,'-------------------',process.env.REACT_APP_SERVER);
+
+export const API = axios.create({
+  baseURL: process.env.REACT_APP_SERVER ? process.env.REACT_APP_SERVER : '/'
+  // baseURL: 'http://localhost:3000'
+});
+
+API.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem('user');
+      store.dispatch({ type: LOGIN, payload: null });
+      window.location.href = config.basename + 'login';
+    }
+
+    if (error.response?.data?.message) {
+      error.message = error.response.data.message;
+    }
+
+    showError(error);
+  }
+);
