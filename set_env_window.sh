e21b17bf4ebb041a171b4493aae8c02306cb328f
goodhzy <goodhzy@gmail.com>
2024-06-27 01:00:02 +0800

    移除sh文件

--- set_env_window.sh
+++
@@ -1,8 +0,0 @@
-$env:REDIS_CONN_STRING = "redis://localhost:6379"
-$env:SESSION_SECRET = "goodhzy00"
-$env:SQL_DSN = "root:123456@tcp(localhost:3306)/ballStarCard"
-$env:SYNC_FREQUENCY = "1800"
-
-
-# 执行命令
-# air.exe -c .air_win.toml
