d59ccd7ffc9f5d5ed002c6096d641a8279838aed
ornnxp1 <1693120570@qq.com>
2024-07-19 10:04:34 +0800

    :rocket: ebay订阅

---
+++ web/berry/src/views/EbayControl/PublishEbay.js
@@ -0,0 +1,24 @@
+import { useState, useEffect } from 'react';
+import { showError, showSuccess } from 'utils/common';
+
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableContainer from '@mui/material/TableContainer';
+import PerfectScrollbar from 'react-perfect-scrollbar';
+import TablePagination from '@mui/material/TablePagination';
+import LinearProgress from '@mui/material/LinearProgress';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import Toolbar from '@mui/material/Toolbar';
+
+import { Button, Card, Box, Stack, Container, Typography } from '@mui/material';
+import { API } from 'utils/api';
+import { ITEMS_PER_PAGE } from 'constants';
+import { IconRefresh, IconPlus } from '@tabler/icons-react';
+
+export default function PublishEbay() {
+
+  return(
+    <>
+    </>
+  )
+}
--- web/berry/src/views/EbayControl/index.js
+++ web/berry/src/views/EbayControl/index.js
@@ -1,9 +1,11 @@
 import { useState, useEffect,useMemo  } from 'react';
 import PropTypes from 'prop-types';
 import { Tabs, Tab, Box, Card } from '@mui/material';
-import { IconSettings2, IconActivity, IconSettings,  IconChartCandle } from '@tabler/icons-react';
+import {  IconInnerShadowTop, } from '@tabler/icons-react';
 import AdminContainer from 'ui-component/AdminContainer';
 import { useLocation, useNavigate } from 'react-router-dom';
+import PublishEbay from './PublishEbay';
+
 
 function CustomTabPanel(props) {
     const { children, value, index, ...other } = props;
@@ -20,3 +22,58 @@ CustomTabPanel.propTypes = {
     index: PropTypes.number.isRequired,
     value: PropTypes.number.isRequired
 };
+
+function a11yProps(index) {
+    return {
+        id: `setting-tab-${index}`,
+        'aria-controls': `setting-tabpanel-${index}`
+    };
+}
+
+const EbayControl = () => {
+    const location = useLocation();
+    const navigate = useNavigate();
+    const hash = location.hash.replace('#', '');
+    const tabMap =  useMemo(() => ({
+        publish: 0,
+    }), []);
+    const [value, setValue] = useState(tabMap[hash] || 0);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+        const hashArray = Object.keys(tabMap);
+        navigate(`#${hashArray[newValue]}`);
+    };
+
+    useEffect(() => {
+        const handleHashChange = () => {
+            const hash = location.hash.replace('#', '');
+            setValue(tabMap[hash] || 0);
+        };
+        window.addEventListener('hashchange', handleHashChange);
+        return () => {
+            window.removeEventListener('hashchange', handleHashChange);
+        };
+    }, [location, tabMap]);
+
+    return(
+      <>
+          <Card>
+              <AdminContainer>
+              <Box sx={{width:'100%'}}>
+                  <Box sx={{ bFBottom: 1, borderColor: 'divider' }}>
+                      <Tabs value={value} onChange={handleChange} variant="scrollable" scrollButtons="auto">
+                          <Tab label="ebay刊登" {...a11yProps(0)} icon={<IconInnerShadowTop />} iconPosition="start" />
+                      </Tabs>
+                  </Box>
+                  <CustomTabPanel value={value} index={0}>
+                      <PublishEbay />
+                  </CustomTabPanel>
+              </Box>
+              </AdminContainer>
+          </Card>
+      </>
+    )
+}
+
+export default EbayControl;
